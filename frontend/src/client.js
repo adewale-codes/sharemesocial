e9676f63905f920fe9600fca2b0e6528c64d4c19
adewale-codes <53108704+adewale-codes@users.noreply.github.com>
2022-11-15 17:42:24 +0100

    fixed login and started sidebar

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,13 +1,12 @@
 import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
-import { version } from 'react'
 
 export const client = sanityClient({
-    projectId: process.eventNames.REACT_APP_SANITY_PROJECT_ID,
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '2022-11-8',
+    apiVersion: '2022-11-08',
     useCdn: true,
-    token: process.eventNames.REACT_APP_SANITY_TOKEN
+    token: process.env.REACT_SANITY_TOKEN
 })
 
 const builder = imageUrlBuilder(client)
